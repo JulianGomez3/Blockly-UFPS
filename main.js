fabfb24b031126d55c758a8ea20f3f8812036883
JulianGomez3 <37245368+JulianGomez3@users.noreply.github.com>
2023-06-21 06:04:59 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const { app, BrowserWindow } = require('electron');
+const express = require('express');
 const path = require('path');
 
 let mainWindow;
 
 function createWindow() {
+  const expressApp = express();
+  expressApp.use(express.static(path.join(__dirname, 'src')));
+
+  const server = expressApp.listen(3000, () => {
+    console.log('Servidor iniciado en http://localhost:8080');
+  });
+
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -12,11 +20,10 @@ function createWindow() {
     }
   });
 
-  const indexPath = path.join(__dirname, 'src', 'index.html');
-
-  mainWindow.loadFile(indexPath);
+  mainWindow.loadURL('http://localhost:8080');
 
   mainWindow.on('closed', () => {
+    server.close();
     mainWindow = null;
   });
 }
