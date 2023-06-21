52676c69d3c32c84901bbafdbac8dba1dfe6069b
JulianGomez3 <37245368+JulianGomez3@users.noreply.github.com>
2023-06-21 01:46:10 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  const indexPath = path.join(__dirname, 'src', 'index.html');
+
+  mainWindow.loadFile(indexPath);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
